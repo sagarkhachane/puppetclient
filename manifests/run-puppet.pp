70d37511851cec6c8b1188fd59dc18fe8641e5ec
sagarkhachane <sagar_khachane@yahoo.co.in>
2019-07-29 08:56:15 -0400

    provided fully qualofied path to run-puppet.pp

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -1,12 +1,12 @@
 # set up regular Puppet run
 
-file { 'usr/local/bin/run-puppet':
+file { '/usr/local/bin/run-puppet':
   source => '/etc/puppetlabs/code/environments/production/files/run-puppet.sh',
   mode   => '0755',
 }
 
 cron { 'run-puppet':
-  command => 'usr/local/bin/run-puppet',
+  command => '/usr/local/bin/run-puppet',
   hour    => '*',
   minute  => '*/15',
 }
