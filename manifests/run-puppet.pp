2bab93d471c9289a9bb2215ab4cc23de85aafd7e
sagarkhachane <sagar_khachane@yahoo.co.in>
2019-07-29 08:53:12 -0400

    modified run-puppet.pp

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -1,2 +1,12 @@
+# set up regular Puppet run
 
-#first file
+file { 'usr/local/bin/run-puppet':
+  source => '/etc/puppetlabs/code/environments/production/files/run-puppet.sh',
+  mode   => '0755',
+}
+
+cron { 'run-puppet':
+  command => 'usr/local/bin/run-puppet',
+  hour    => '*',
+  minute  => '*/15',
+}
